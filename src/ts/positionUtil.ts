af79dece78fb58661b7e77b7779bed524a008f4d
Nick Krichevsky <nick@ollien.com>
2020-03-03 22:43:36 -0500

    Add ability to track position of a pipe

--- src/ts/positionUtil.ts
+++ src/ts/positionUtil.ts
@@ -1,3 +1,6 @@
+import lodash from 'lodash';
+import { Matrix } from 'ml-matrix';
+import * as mlMatrix from 'ml-matrix';
 // @ts-ignore There are no definitions for quaternion
 import Quaternion from 'quaternion';
 
@@ -21,18 +24,61 @@ export function degreesToRadians(angle: number): number {
 	return angle * (Math.PI / 180);
 }
 
-// TODO: This may not be what we want. We want some way for pipes to rotate in place. Need to futz.
-//       Currently this rotates fully about the origin but since we're tracking pipes, we really need to rotate those pipes in place.
+/**
+ * Perform componentwise operation on the given triplets
+ * @param a The left operand of the subtraction
+ * @param b The right operand of the subtraction
+ * @param op The operation to perform.
+ */
+function operateOnTriplets(
+	a: Triplet<number>,
+	b: Triplet<number>,
+	op: (a: number, b: number) => number,
+): Triplet<number> {
+	return <Triplet<number>> lodash.zip(a, b).map((components: [number?, number?]): number => {
+		const aComponent: number = components[0]!;
+		const bComponent: number = components[1]!;
+
+		return op(aComponent, bComponent);
+	});
+}
+
+/**
+ * Generate the path taken by a the rotations applied to a pipe
+ * @param startingPoint The point the pipe will start with
+ * @param rotations The rotations applied to the pipe
+ */
 export function generateTrailFromRotations(
 	startingPoint: Triplet<number>,
 	rotations: SpatialRotation[],
 ): Triplet<number>[] {
 	const trail: Triplet<number>[] = [startingPoint];
+	// This matrix represents a change of basis from the standard coordinates to the coordinates of each pipe
+	// As explained in the shader, each pipe has its own local y axis, which means that we must perform all rotations
+	// with respect to this "local" axis. e.g. no y axis rotations actually do anything but spin the cylinder
+	// (which in reality, has no discernable effect).
+	const basisMatrix = Matrix.identity(3);
 
+	const growthDirection: Triplet<number> = [0, 1, 0];
 	let cursor = startingPoint;
 	rotations.forEach((rotation: SpatialRotation) => {
-		const q = Quaternion.fromAxisAngle(rotation.axis, degreesToRadians(rotation.angle));
-		cursor = q.rotateVector(cursor);
+		const quaternion = Quaternion.fromAxisAngle(rotation.axis, degreesToRadians(rotation.angle));
+		// Rotate each part of the basis in accordance with the quaternion
+		for (let i = 0; i < basisMatrix.columns; i++) {
+			const axis = <Triplet<number>>basisMatrix.getColumn(i);
+			const rotatedAxis = quaternion.rotateVector(axis);
+			basisMatrix.setColumn(i, rotatedAxis);
+		}
+
+		const growthDirectionVector = (new Matrix([growthDirection])).transpose();
+		// Convert our change of basis matrix into the inverse, allowing us to move from standard coordinates to
+		// the coordinates created by the system.
+		const inverseBasis = mlMatrix.inverse(basisMatrix);
+		const reorientedDirectionVector = <Triplet<number>>inverseBasis.mmul(growthDirectionVector).getColumn(0);
+
+		// Use this offset to adjust our cursor.
+		cursor = operateOnTriplets(cursor, reorientedDirectionVector, (a, b) => a + b);
+
 		trail.push(cursor);
 	});
 
--- src/ts/tests/positionUtil.spec.ts
+++ src/ts/tests/positionUtil.spec.ts
@@ -6,41 +6,91 @@ import { Triplet } from '../positionUtil'; // eslint-disable-line no-unused-vars
 import * as positionUtil from '../positionUtil';
 
 describe('positionUtil', () => {
-	it('should correctly generate a path given a rotation set', () => {
-		const startingPoint: Triplet<number> = [1, 1, 0];
-		const rotations: positionUtil.SpatialRotation[] = [
-			{
-				angle: -90,
-				axis: [0, 1, 0],
-			},
-			{
-				angle: -90,
-				axis: [0, 0, 1],
-			},
-			{
-				angle: -90,
-				axis: [1, 0, 0],
-			},
-		];
+	const pathTests = [
+		{
+			name: 'all positive',
+			rotations: [
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 0, 1] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+			],
+			expected: [
+				[1, 1, 0],
+				[1, 2, 0],
+				[1, 2, 1],
+				[1, 2, 2],
+				[1, 1, 2],
+			],
+		},
+		{
+			name: 'loop',
+			rotations: [
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+			],
+			expected: [
+				[1, 1, 0],
+				[1, 1, -1],
+				[1, 0, -1],
+				[1, 0, 0],
+				[1, 1, 0],
+			],
+		},
+		{
+			name: 'one bend',
+			rotations: [
+				{ angle: 90, axis: <Triplet<number>>[1, 0, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+			],
+			expected: [
+				[1, 1, 0],
+				[1, 1, -1],
+				[1, 1, -2],
+				[1, 1, -3],
+				[1, 1, -4],
+			],
+		},
+		{
+			name: 'no bends',
+			rotations: [
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+				{ angle: 90, axis: <Triplet<number>>[0, 1, 0] },
+			],
+			expected: [
+				[1, 1, 0],
+				[1, 2, 0],
+				[1, 3, 0],
+				[1, 4, 0],
+			],
+		},
+	];
 
-		const actualPath = positionUtil.generateTrailFromRotations(startingPoint, rotations);
-		const expectedPath = [
-			[1, 1, 0],
-			[0, 1, 1],
-			[1, 0, 1],
-			[1, 1, 0],
-		];
-		actualPath.forEach((_, stepIndex: number) => {
-			actualPath[stepIndex].forEach((__, componentIndex: number) => {
-				const actual = actualPath[stepIndex][componentIndex];
-				const expected = expectedPath[stepIndex][componentIndex];
-				assert.closeTo(
-					actual,
-					expected,
-					0.001,
-					`Component ${componentIndex} of step ${stepIndex} does not match:`
-					+ `should be [${expectedPath[stepIndex]}], is [${actualPath[stepIndex]}]`,
-				);
+	pathTests.forEach((spec) => {
+		it(`should correctly generate a path given a rotation set: ${spec.name}`, () => {
+			const startingPoint: Triplet<number> = [1, 1, 0];
+			// eslint-disable-next-line prefer-destructuring
+			const rotations: positionUtil.SpatialRotation[] = spec.rotations;
+			const actualPath = positionUtil.generateTrailFromRotations(startingPoint, rotations);
+			const expectedPath = spec.expected;
+
+			actualPath.forEach((_, stepIndex: number) => {
+				actualPath[stepIndex].forEach((__, componentIndex: number) => {
+					const actual = actualPath[stepIndex][componentIndex];
+					const expected = expectedPath[stepIndex][componentIndex];
+					assert.closeTo(
+						actual,
+						expected,
+						0.001,
+						`Component ${componentIndex} of step ${stepIndex} does not match:`
+						+ `should be [${expectedPath[stepIndex]}], is [${actualPath[stepIndex]}]`,
+					);
+				});
 			});
 		});
 	});
